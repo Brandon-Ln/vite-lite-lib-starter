4cc6243263e3111a2db8796bb1c71d4ac92115a0
Brandon Leon <awesome163@126.com>
2023-02-26 23:47:37 +0800

    feat(example): update example code, import src export
    NA

--- example/entry.tsx
+++ example/entry.tsx
@@ -1,3 +1,10 @@
+import { sum } from '../src/index'
+
 const root = document.getElementById('root')!
 
-root.innerHTML = '<h1>Vite lite lib Starter</h1>'
+root.innerHTML = `
+<div>
+<h1>Vite lite lib Starter</h1>
+<p>1 + 2 + 3 + 4 = ${sum(1, 2, 3, 4)}</p>
+</div>
+`
